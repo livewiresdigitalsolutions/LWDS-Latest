3595eff9ae785b55017a466171f37bab957791fc
Mohammed Yasar <109421595+Yaz03@users.noreply.github.com>
2026-09-17 23:10:56 +0530

    fix: add @source to Tailwind CSS to ensure classes are parsed

--- .next/static/chunks/app/layout-5f5bc96d0841c437.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7001:()=>{},9404:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,7001,23))}},e=>{e.O(0,[877,441,255,358],()=>e(e.s=9404)),_N_E=e.O()}]);
